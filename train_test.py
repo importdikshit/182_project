574da0e078dc397a08b49e298e07c5be86ba1e68
id73 <ishan.d@berkeley.edu>
2019-05-02 22:16:34 -0700

    added sudos to sript

--- train_test.py
+++ train_test.py
@@ -13,27 +13,28 @@ if __name__ == "__main__":
     directories = ["./data/train/" + this_root + "/" for this_root in this_file]
     directories += ["./data/val/" + this_root + "/" for this_root in this_file]
 
+    for this_dir in directories:
+        os.system("sudo mkdir " + this_dir)
+
     root_dir = "./data/images/"
 
-root_dir = "./data/images/"
+    for this_folder in os.listdir(root_dir):
 
-for this_folder in os.listdir(root_dir):
+        if this_folder[0] != '.':
 
-    if this_folder[0] != '.':
+            all_images = np.array(os.listdir(os.path.join(root_dir, this_folder)))
+            np.random.shuffle(all_images)
 
-        all_images = np.array(os.listdir(os.path.join(root_dir, this_folder)))
-        np.random.shuffle(all_images)
+            first = int(len(all_images)*0.85)
 
-        first = int(len(all_images)*0.85)
+            train_dat = all_images[:first]
+            val_dat = all_images[first:]
 
-        train_dat = all_images[:first]
-        val_dat = all_images[first:]
-
-        for this_image in train_dat:
-            move_to = "./data/train/"
-            os.system("mv " + os.path.join(root_dir, this_folder, this_image) + " " + os.path.join(move_to, this_folder, this_image))
+            for this_image in train_dat:
+                move_to = "./data/train/"
+                os.system("sudo mv " + os.path.join(root_dir, this_folder, this_image) + " " + os.path.join(move_to, this_folder, this_image))
 
 
-        for this_image in val_dat:
-            move_to = "./data/val/"
-            os.system("mv " + os.path.join(root_dir, this_folder, this_image) + " " + os.path.join(move_to, this_folder, this_image))
+            for this_image in val_dat:
+                move_to = "./data/val/"
+                os.system("sudo mv " + os.path.join(root_dir, this_folder, this_image) + " " + os.path.join(move_to, this_folder, this_image))
