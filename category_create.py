c7591a9cebedcd076218494ad857def674a2fed9
id73 <ishan.d@berkeley.edu>
2019-05-02 18:13:29 -0700

    add bash script

---
+++ category_create.py
@@ -0,0 +1,38 @@
+import os
+
+if __name__ == "__main__":
+
+    this_file = open("./data/labels/list_category_cloth.txt", "r").read()
+    this_file = this_file.split('\n')
+    this_file = this_file[2:]
+    this_file = [this_one.split(' ')[0] for this_one in this_file][:-1]
+    assert(len(this_file)==50)
+
+    directories = ["./data/images/" + this_root + "/" for this_root in this_file]
+    os.system("sudo mkdir ./data/images/")
+
+
+    # Create all the subdirectories
+    for this_dir in directories:
+        os.system("sudo mkdir " + this_dir)
+
+
+    # Move all the files by category
+    for this_dir in tqdm.tqdm(os.listdir("./data/img/")):
+
+        if this_dir[0] != ".":
+
+            type_article = this_dir.split("_")[-1]
+
+            if "&" in this_dir:
+                this_dir = this_dir.replace("&", "\&")
+                this_dir = this_dir.replace(";", "\;")
+    #             this_dir = this_dir.replace("'", "\\'")
+
+            os.system("mv ./data/img/" + this_dir + "/* ./data/images/" + type_article + "/")
+
+
+    for this_dir in os.listdir("./data/images/"):
+
+        # Print the number of files in each directory
+        print(this_dir, len(os.listdir("./data/images/" + this_dir + "/")))
